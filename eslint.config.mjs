5885937ea061c7badbec60ff352bd579023d6f47
neelcc <neel.c.chaurasiya@gmail.com>
2026-02-06 20:28:53 +0530

    testing

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,23 @@
 // @ts-check
 
-import eslint from '@eslint/js';
-import { defineConfig } from 'eslint/config';
-import tseslint from 'typescript-eslint';
+import eslint from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
 
 export default defineConfig(
-  eslint.configs.recommended,
-  tseslint.configs.recommendedTypeChecked,
-  {
-    ignores: ["dist/**","node_modules/**",'eslint.config.mjs']
-  },
-  {
-    languageOptions: {
-      parserOptions: {
-        projectService: true,
-      },
+    eslint.configs.recommended,
+    tseslint.configs.recommendedTypeChecked,
+    {
+        ignores: ["dist/**", "node_modules/**", "eslint.config.mjs"],
     },
-    rules : {
-      "dot-notation" : 'error'
-     },
-  },
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+            },
+        },
+        rules: {
+            "dot-notation": "error",
+        },
+    },
 );
